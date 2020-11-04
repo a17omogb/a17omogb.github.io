874618ad83bb83075ee67def8408af4da9fbd2f2
a17omogb <57695301+a17omogb@users.noreply.github.com>
2020-11-04 12:59:02 -0500

    server stuff

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -17,16 +18,19 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async(req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json ;
+    console.log('fetch request data', json);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
+
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+
+    res.send('Hello World!');
   });
 
 app.listen(port, () => {
