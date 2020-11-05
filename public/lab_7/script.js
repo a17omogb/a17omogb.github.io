54d788d145637dbbb1e8c6c73025a546a0158c3e
a17omogb <57695301+a17omogb@users.noreply.github.com>
2020-11-04 23:30:03 -0500

    lab 7 changes
    these are the changes for lab 7

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,19 +1,45 @@
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
-  return list;
+  const categoryArray = [];
+  const result = {};
+  for (let i = 0; i < restaurantList.length; i += 1) {
+    categoryArray.push(restaurantList[i].category);
+  }
+
+  for (let i = 0; i < categoryArray.length; i += 1) {
+    if (!result[categoryArray[i]]) {
+      result[categoryArray[i]] = 0;
+    }
+    result[categoryArray[i]] += 1;
+  }
+
+  const reply = Object.keys(result).map((category) => ({
+    y: result[category], 
+    label: category
+  }));
+
+  console.log('reply', reply);
+  return reply;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
+  console.log('makeYourOptionsObject')
+
   CanvasJS.addColorSet('customColorSet1', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    "#2F4F4F",
+    "#008080",
+    "#2E8B57",
+    "#3CB371",
+    "#90EE90"
   ]);
 
   return {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Places to eat out in future'
     },
     axisX: {
       interval: 1,
@@ -24,7 +50,26 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
       gridColor: 'rgba(1,77,101,.1)',
       title: 'Change This Title',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {customBreaks: [
+        {
+          type: 'zigzag',
+          startValue: 40,
+          endValue: 50,
+          color: 'blue'
+        },
+        {
+          type: 'zigzag',
+          startValue: 85,
+          endValue: 100,
+          color: 'blue'
+        },
+        {
+          type: 'zigzag',
+          startValue: 140,
+          endValue: 175,
+          color: 'blue'
+        }
+      ]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
