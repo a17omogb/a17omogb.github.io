11851fdc8bda93a8fac3019c692f9dd8f3067b69
a17omogb <57695301+a17omogb@users.noreply.github.com>
2020-11-04 23:49:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
+
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
@@ -15,27 +16,27 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
+});
 
 app.route('/api')
-  .get(async(req, res) => {
+  .get((req, res) => {
     console.log('GET request detected');
     console.log('fetch request data', json)
   })
-
   .post(async(req, res) => {
-
-
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    const json = await data.json() ;
-    console.log('fetch request data',json);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json%27)
+    const json = await data.json();
+    console.log('fetch request data', json);
     res.json(json);
+
     console.log('POST request detected');
+    //console.log('Form data in res.body');
 
   });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(Example app listening on port ${port}!);
 });
